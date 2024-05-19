d1006aa315d417f5399ac8f57740708ec9d7e8cc
Hiimuroo <matthieu.labille@gmail.com>
2024-05-19 08:45:41 +0200

    modified:   src/Pages/Home.js 	modified:   src/assets/style/Home.css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import '../assets/style/Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faTwitter, faFacebook, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   const [showPseudo, setShowPseudo] = useState(false);
@@ -15,10 +15,7 @@ const Home = () => {
       <div className="social-links">
         <a href="https://github.com/Hiimuroo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
         <a href="https://www.linkedin.com/in/matthieulabille/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="https://github.com/Hiimuroo"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a href="https://github.com/Hiimuroo"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a href="https://github.com/Hiimuroo"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href="javascript:void(0)" onClick={() => setShowPseudo(!showPseudo)}><FontAwesomeIcon icon={faDiscord} /></a>
+        <button onClick={() => setShowPseudo(!showPseudo)}><FontAwesomeIcon icon={faDiscord} /></button>
         {showPseudo && (
           <span className="discord-pseudo">himuroo</span>
         )}
