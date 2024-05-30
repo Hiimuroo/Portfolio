9fde48f78e2d39e1304f5e2881329682df80c05d
Hiimuroo <matthieu.labille@gmail.com>
2024-05-30 18:10:29 +0200

    modified:   src/assets/style/Navbar.css 	modified:   src/assets/style/Skills.css 	modified:   src/components/Skills.js 	modified:   src/index.css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3Alt, faSass, faJs, faReact, faNodeJs, faGithub, faBootstrap, faGoogle, faTrello, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3Alt, faSass, faJs, faReact, faNodeJs, faBootstrap, faPython } from '@fortawesome/free-brands-svg-icons';
 import reduxLogo from '../assets/images/redux.svg';
 import JestLogo from '../assets/images/jest.png';
 import MongoLogo from '../assets/images/Mongo.png';
@@ -10,7 +10,6 @@ const Skills = () => {
   const [loaded, setLoaded] = useState(false);
   const [showColumn1, setShowColumn1] = useState(false);
   const [showColumn2, setShowColumn2] = useState(false);
-  const [showColumn3, setShowColumn3] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -28,13 +27,9 @@ const Skills = () => {
       const columnTimer2 = setTimeout(() => {
         setShowColumn2(true);
       }, 400);
-      const columnTimer3 = setTimeout(() => {
-        setShowColumn3(true);
-      }, 500);
       return () => {
         clearTimeout(columnTimer1);
         clearTimeout(columnTimer2);
-        clearTimeout(columnTimer3);
       }
     }
   }, [loaded]);
@@ -48,44 +43,39 @@ const Skills = () => {
           <div className={`column ${showColumn1 ? 'show' : ''}`}>
             <h3>FRONT-END</h3>
             <div className="icons">
-              <FontAwesomeIcon icon={faHtml5} className="skill-icon" />
-              <span>HTML5</span>
-              <FontAwesomeIcon icon={faCss3Alt} className="skill-icon" />
-              <span>CSS3</span>
-              <FontAwesomeIcon icon={faSass} className="skill-icon" />
-              <span>Sass</span>
-              <FontAwesomeIcon icon={faJs} className="skill-icon" />
-              <span>JavaScript</span>
-              <FontAwesomeIcon icon={faBootstrap} className="skill-icon" />
-              <span>Bootstrap</span>
-              <FontAwesomeIcon icon={faReact} className="skill-icon" />
-              <span>React</span>
+              <div className="sub-column">
+                <FontAwesomeIcon icon={faHtml5} className="skill-icon" />
+                <span>HTML5</span>
+                <FontAwesomeIcon icon={faCss3Alt} className="skill-icon" />
+                <span>CSS3</span>
+                <FontAwesomeIcon icon={faSass} className="skill-icon" />
+                <span>Sass</span>
+                <FontAwesomeIcon icon={faJs} className="skill-icon" />
+                <span>JavaScript</span>
+              </div>
+              <div className="sub-column">
+                <FontAwesomeIcon icon={faBootstrap} className="skill-icon" />
+                <span>Bootstrap</span>
+                <FontAwesomeIcon icon={faReact} className="skill-icon" />
+                <span>React</span>
+                <img src={reduxLogo} alt="Redux" className="image-icon" />
+                <span>Redux</span>
+              </div>
             </div>
           </div>
           <div className={`column ${showColumn2 ? 'show' : ''}`}>
             <h3>BACK-END</h3>
             <div className="icons">
-            <FontAwesomeIcon icon={faNodeJs} className="skill-icon" />
-              <span>Node.js</span>
-              <img src={MongoLogo} alt="MongoDB" className="skill-icon" />
-              <span>MongoDB</span>
-              <img src={JestLogo} alt="Jest" className="skill-icon" />
-              <span>Jest</span>
-              <img src={reduxLogo} alt="Redux" className="skill-icon" />
-              <span>Redux</span>
-              <FontAwesomeIcon icon={faPython} className="skill-icon" />
-              <span>Python</span>
-            </div>
-          </div>
-          <div className={`column ${showColumn3 ? 'show' : ''}`}>
-            <h3>Outils & Logiciels</h3>
-            <div className="icons">
-              <FontAwesomeIcon icon={faGithub} className="skill-icon" />
-              <span>GitHub</span>
-              <FontAwesomeIcon icon={faGoogle} className="skill-icon" />
-              <span>Lighthouse</span>
-              <FontAwesomeIcon icon={faTrello} className="skill-icon" />
-              <span>Trello</span>
+              <div className="sub-column">
+                <FontAwesomeIcon icon={faNodeJs} className="skill-icon" />
+                <span>Node.js</span>
+                <img src={MongoLogo} alt="MongoDB" className="image-icon" />
+                <span>MongoDB</span>
+                <img src={JestLogo} alt="Jest" className="image-icon" />
+                <span>Jest</span>
+                <FontAwesomeIcon icon={faPython} className="skill-icon" />
+                <span>Python</span>
+              </div>
             </div>
           </div>
         </div>
